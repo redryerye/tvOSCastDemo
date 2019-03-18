15e99edea1bdc1baff04495530835a2a09fe15dd
yuki <yymmt3.waste@gmail.com>
2019-03-18 21:06:31 +0900

    initial commit

---
+++ tvOSReceiver/tvOSReceiver/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  tvOSReceiver
+//
+//  Created by Yuki Yamamoto on 2019/03/18.
+//  Copyright © 2019 Yuki Yamamoto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+    }
+
+
+}
+
